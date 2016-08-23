7e8417d22189313f0d26c35914dc85b7b3ff4265
Alexander Maksimyak <alexander.max@gmail.com>
2016-08-23 11:38:03 +0300

    Update tets (allows guest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,14 @@ feature "Contact Creation" do
   scenario "allows acess to contacts page" do
     visit '/contacts'
 
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content I18n.t('contacts.contact_us')
+  end
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'Something'
+    click_button 'Save Contact'
+    expect(page).to have_content 'Thanks'
   end
 end
